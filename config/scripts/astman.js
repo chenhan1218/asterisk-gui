0ced9c5d77bee07ac32b032ea63cee391435013b
pari <pari@f2be6d49-a0cf-43a2-a903-cc364acb77d4>
2007-02-16 06:44:49 +0000

    fix: Save and cancel buttons are being enabled When keys like CTRL or CAPSLOCK are pressed
    git-svn-id: http://svn.digium.com/svn/asterisk-gui/trunk@351 f2be6d49-a0cf-43a2-a903-cc364acb77d4

--- config/scripts/astman.js
+++ config/scripts/astman.js
@@ -1496,7 +1496,7 @@ function Astman() {
 							return true;
 						}
 						widgets[x].onkeyup = function(event) {
-							if (event.keyCode ==9){return true;}
+							if (this.oldvalue == this.value){return true;}
 							pattern = this.getAttribute('pattern');
 							if (pattern && check_pattern(pattern, this.oldvalue) && !check_pattern(pattern, this.value)) {
 									this.value = this.oldvalue;
